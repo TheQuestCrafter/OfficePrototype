1ec36394a1743fc7affc7ba7dc9acd95b38c7f29
carpentre1 <jlowell.carpenter@gmail.com>
2019-02-16 18:28:24 -0600

    Added office area
    Player can move around the office
    Player can push Jim out of the way
    Added a few boundary boxes near Michael's office, just to test and help get a sense of scale
    
    TODO: interact with NPC to enter chili minigame, earn coins from game

---
+++ Assets/Scripts/OfficePlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfficePlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 10f;
+
+    private float horizontal;
+    private float vertical;
+
+    [SerializeField]
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+    }
+}
