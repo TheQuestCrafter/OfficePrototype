d3ac6e675571a894c5d20e16a85a4bedb8f8087d
Jiening <Jiening.tao@loop.colum.edu>
2019-02-18 20:07:52 -0600

    Proximity speech added

---
+++ Assets/Scripts/NPCScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCScript : MonoBehaviour
+{
+
+    [SerializeField]
+    private TextMesh speech;
+    [SerializeField]
+    private string ProximitySpeech;
+
+
+    private Collider2D ProximityCheck = new Collider2D();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speech.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            speech.text = ProximitySpeech;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        speech.text = "";
+    }
+}
