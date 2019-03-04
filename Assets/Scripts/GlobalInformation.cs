90a743078a8ad79178d58d11416ba9fa72b8e92a
TheQuestCrafter <thequestcrafter@gmail.com>
2019-03-04 17:33:51 -0600

    QuestSystem now works between scenes

--- Assets/Scripts/GlobalInformation.cs
+++ Assets/Scripts/GlobalInformation.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class GlobalInformation : MonoBehaviour
 {
     public int Shrutebucks;
+    private bool GMActive;
+    private GlobalInformation GM;
     void Start()
     {
+        GM = (GlobalInformation)FindObjectOfType(typeof(GlobalInformation));
+        if (GM.GMActive == true)
+        {
+            Destroy(this.gameObject);
+        }
+        GMActive = true;
         DontDestroyOnLoad(this);
     }
 }
