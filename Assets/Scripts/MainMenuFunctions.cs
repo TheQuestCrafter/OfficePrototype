16df8e8e6b3eb87d5a4f6ed65913af25ff0777b3
TheQuestCrafter <thequestcrafter@gmail.com>
2019-04-02 00:19:18 -0500

    Basic Main Menu

---
+++ Assets/Scripts/MainMenuFunctions.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuFunctions : MonoBehaviour
+{
+    [Tooltip("This int transfers you to the sceneIndex with that number")]
+    [SerializeField]
+    private int sceneSelected = 1;
+
+    public void MainMenuSceneSelect()
+    {
+        SceneManager.LoadScene(sceneSelected);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
