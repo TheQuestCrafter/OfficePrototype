74331ab2ec8353e58251f4d4230b8dc1e822cf3f
TheQuestCrafter <thequestcrafter@gmail.com>
2019-04-03 21:19:43 -0500

    Basic Main Menu with Key Controls

---
+++ Assets/Scripts/MenuScreen/MainMenuFunctions.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenuFunctions : MonoBehaviour
+{
+    [Tooltip("This int transfers you to the sceneIndex with that number")]
+    [SerializeField]
+    private int sceneSelected = 1;
+    [Tooltip("Gives the event system to the script to find buttons.")]
+    [SerializeField]
+    private EventSystem eventSystem;
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip selectedClip;
+    [SerializeField]
+    private GameObject firstButton;
+
+    private int firstSelected;
+
+    void Start()
+    {
+        firstSelected = 0;
+    }
+
+    public void PlaySelectSound()
+    {
+        if (firstButton == eventSystem.currentSelectedGameObject && firstSelected == 0)
+        {
+            firstSelected++;
+        }
+        else
+        {
+            Debug.Log("Sound Played");
+            audioSource.PlayOneShot(selectedClip);
+        }
+    }
+
+    public void MainMenuSceneSelect()
+    {
+        SceneManager.LoadScene(sceneSelected);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MenuScreen/MenuEnabling.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuEnabling : MonoBehaviour
+{
+    [Tooltip("Gives the event system to the script to find buttons.")]
+    [SerializeField]
+    private EventSystem eventSystem;
+
+    [SerializeField]
+    private GameObject selectedButton;
+
+    private bool buttonSelected;
+
+    public void ReEnableMenu()
+    {
+        eventSystem.SetSelectedGameObject(selectedButton);
+        buttonSelected = true;
+    }
+}
