e5fdd7fe95d02fdded6dbe3ceab7b4082c957d4d
TheQuestCrafter <thequestcrafter@gmail.com>
2019-04-10 00:10:48 -0500

    Basic Pause Menu & Font Some Implementation

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    [SerializeField]
+    private GameObject pauseMenuPanel;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        pauseMenuPanel.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+        
+    }
+
+    public void Resume()
+    {
+        pauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
