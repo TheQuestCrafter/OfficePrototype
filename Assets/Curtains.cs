af47fa2580ffb5aa339d1789e25659d294b639c2
Jiening <Jiening.tao@loop.colum.edu>
2019-04-10 14:34:37 -0500

    updated sprites and colliders, no more bring your child too work day

---
+++ Assets/Curtains.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Curtains : MonoBehaviour
+{
+    Vector3 currentPosition;
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    bool moveRight = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPosition = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if(moveRight)
+            currentPosition.x =  speed;
+        else
+            currentPosition.x =  -(speed);
+
+        transform.position += currentPosition;
+
+        if (Time.fixedTime >= 7)
+            gameObject.SetActive(false);
+
+
+    }
+}
