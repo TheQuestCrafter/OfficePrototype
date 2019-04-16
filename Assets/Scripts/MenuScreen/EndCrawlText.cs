4571c8b43c110b818781707103c170e712d55c67
parkerhams <parker.hamilton@loop.colum.edu>
2019-04-16 18:32:19 -0500

    Finished the crawl text and then added it to the build settings

---
+++ Assets/Scripts/MenuScreen/EndCrawlText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndCrawlText : MonoBehaviour
+{
+    [Tooltip("This is the opening crawl text animation")]
+    [SerializeField]
+    private Animator textCrawl;
+
+    [SerializeField]
+    private string dayScene;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(textCrawl.GetCurrentAnimatorStateInfo(0).IsName("TextCrawlDone")) //if animation clip has ended
+        {
+            SceneManager.LoadScene(dayScene);
+        }
+    }
+}
