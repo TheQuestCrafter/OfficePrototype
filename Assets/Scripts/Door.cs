9020499e011eecfd2bd611b792e45e93a2fbdcdc
TheQuestCrafter <thequestcrafter@gmail.com>
2019-05-04 15:07:09 -0500

    Revert "Revert "fixed door blinking issue""
    This reverts commit 2612126082e909474b99aad904b58467288ee19b.

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -8,15 +8,19 @@ public class Door : MonoBehaviour
     [SerializeField]
     private bool locked;
 
+    [SerializeField]
+    private LayerMask playerLayer;
 
 
     private SpriteRenderer mySpriteRenderer;
     private BoxCollider2D doorCollider;
+    private CapsuleCollider2D checkCollider;
 
     private void Start()
     {
         mySpriteRenderer = GetComponent<SpriteRenderer>();
         doorCollider = GetComponent<BoxCollider2D>();
+        checkCollider = GetComponent<CapsuleCollider2D>();
     }
 
 
@@ -24,31 +28,36 @@ public class Door : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")&&!locked)
-        {
-            OpenDoor(true);
-        }
+        CheeckPlayerLeft();
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
-        {
-            OpenDoor(false);
-        }
+        CheeckPlayerLeft();
     }
 
     private void OpenDoor(bool open)
     {
-        if (open)
+        if (!locked)
         {
-            mySpriteRenderer.enabled = false;
-            doorCollider.enabled = false;
+            if (open)
+            {
+                mySpriteRenderer.enabled = false;
+                doorCollider.enabled = false;
+            }
+            else
+            {
+                mySpriteRenderer.enabled = true;
+                doorCollider.enabled = true;
+            }
         }
+    }
+
+    private void CheeckPlayerLeft()
+    {
+        if (Physics2D.OverlapCapsule(transform.position, checkCollider.size, checkCollider.direction, 0, playerLayer) == null)
+            OpenDoor(false);
         else
-        {
-            mySpriteRenderer.enabled = true;
-            doorCollider.enabled = true;
-        }
+            OpenDoor(true);
     }
 }
